9b73b3aa31c7f3f4765ce2dc5350b7da3c51f983
shi-you-ta <shota_hakamada1993@outlook.com>
2024-03-27 19:33:49 +0900

    VisualBasic2022デスクトップアプリ超入門　Chapter7-2改修（HttpClientへ）

---
+++ SlackPost_20240327/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TxtPost = New TextBox()
+        ButPost = New Button()
+        SuspendLayout()
+        ' 
+        ' TxtPost
+        ' 
+        TxtPost.Location = New Point(11, 12)
+        TxtPost.Multiline = True
+        TxtPost.Name = "TxtPost"
+        TxtPost.Size = New Size(259, 185)
+        TxtPost.TabIndex = 0
+        ' 
+        ' ButPost
+        ' 
+        ButPost.Location = New Point(172, 203)
+        ButPost.Name = "ButPost"
+        ButPost.Size = New Size(98, 38)
+        ButPost.TabIndex = 1
+        ButPost.Text = "投稿"
+        ButPost.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(282, 253)
+        Controls.Add(ButPost)
+        Controls.Add(TxtPost)
+        Name = "Form1"
+        Text = "Slack投稿"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TxtPost As TextBox
+    Friend WithEvents ButPost As Button
+
+End Class
---
+++ SlackPost_20240327/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Net.Http
+
+Public Class Form1
+    Private Sub ButPost_Click(sender As Object, e As EventArgs) Handles ButPost.Click
+        Dim strWebHookUrl = "https://hooks.slack.com/services/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"
+        Dim strData = String.Format("{{'text':'{0}'}}", TxtPost.Text)
+        'Dim client = New Net.WebClient()
+        Dim cl = New Net.Http.HttpClient()
+
+        'HttpClientクラスでリクエストヘッダーの追加
+        cl.DefaultRequestHeaders.Add("ContentType", "application/json;charset=UTF-8")
+        'WebClientクラスでリクエストヘッダーの追加
+        'client.Headers.Add(Net.HttpRequestHeader.ContentType, "application/json;charset=UTF-8")
+
+        'HttpClientクラスでリクエストデータのエンコーディングを設定
+        'String値（strData）からhttpContenntに変換
+        Dim content = New StringContent(strData, System.Text.Encoding.UTF8, "application/json")
+        'WebClientクラスでリクエストデータのエンコーディングを設定
+        'client.Encoding = System.Text.Encoding.UTF8
+
+        'HttpClientクラスでPOSTリクエストの送信
+        Dim response = cl.PostAsync(strWebHookUrl, content).Result
+        'HttpClientクラスでレスポンスの読み取り
+        Dim reply = response.Content.ReadAsStringAsync().Result
+        'WebClientクラスでレスポンスの読み取り
+        'Dim reply = client.UploadString(strWebHookUrl, strData)
+
+        MessageBox.Show(reply)
+
+    End Sub
+End Class
